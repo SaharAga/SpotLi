ffd8d0b51c930a64e6c771ba07c2e6737d4fcb2f
Sahar Aga <saharaga97@gmail.com>
2026-08-27 20:24:56 +0300

    fix(functions): Remove enforceAppCheck on gmail functions for unblocked OAuth invocation

--- functions/src/index.js
+++ functions/src/index.js
@@ -73,7 +73,6 @@ export const inboundEmailWebhook = onRequest(
  */
 export const gmailOAuthStart = onCall(
   {
-    enforceAppCheck: true,
     secrets: [gmailOAuthClientSecret],
     timeoutSeconds: 15,
     memory: '128MiB'
@@ -144,7 +143,6 @@ export const gmailPushNotification = onRequest(
  */
 export const gmailBackfill = onCall(
   {
-    enforceAppCheck: true,
     secrets: [gmailOAuthClientSecret],
     // Scanning up to 100 messages one-by-one can take a while.
     timeoutSeconds: 180,
@@ -183,7 +181,6 @@ export const gmailWatchRenewal = onSchedule(
  */
 export const gmailDisconnect = onCall(
   {
-    enforceAppCheck: true,
     secrets: [gmailOAuthClientSecret],
     timeoutSeconds: 30,
     memory: '128MiB'
